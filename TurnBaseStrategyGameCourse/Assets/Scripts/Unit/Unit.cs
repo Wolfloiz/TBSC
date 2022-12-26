1d69e234311edbaff3fc53989fca63a9f5f0ef99
Luiz <lhfmonteiro@sga.pucminas.br>
2022-12-26 20:55:54 -0300

    controlando animações ao andar

--- TurnBaseStrategyGameCourse/Assets/Scripts/Unit/Unit.cs
+++ TurnBaseStrategyGameCourse/Assets/Scripts/Unit/Unit.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+  [SerializeField] private Animator unitAnimator;
 
   private Vector3 targetPosition;
 
   private void Update()
   {
-
+    unitAnimator.SetBool("isWalking", false);
     float stoppinDistance = .1f;
     if (Vector3.Distance(transform.position, targetPosition) > stoppinDistance)
     {
       Vector3 moveDirection = (targetPosition - transform.position).normalized;
       float moveSpeed = 4f;
       transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
+      unitAnimator.SetBool("isWalking", true);
     }
 
     if (Input.GetMouseButtonDown(0))
