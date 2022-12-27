155bca3ba92269cd333752361eff91e9b417b46b
Luiz <lhfmonteiro@sga.pucminas.br>
2022-12-26 21:03:02 -0300

    adicionando rotação a animação de andar

--- TurnBaseStrategyGameCourse/Assets/Scripts/Unit/Unit.cs
+++ TurnBaseStrategyGameCourse/Assets/Scripts/Unit/Unit.cs
@@ -17,6 +17,10 @@ public class Unit : MonoBehaviour
       Vector3 moveDirection = (targetPosition - transform.position).normalized;
       float moveSpeed = 4f;
       transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+      float rotateSpeed = 10f;
+      transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
       unitAnimator.SetBool("isWalking", true);
     }
 
