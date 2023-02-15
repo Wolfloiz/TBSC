2a18389f25d9f39d86a83624467997b597f1e64c
Luiz <lhfmonteiro@sga.pucminas.br>
2023-02-15 00:13:55 -0300

    criando Componente UI, healthBar e ActionPoint

--- TurnBaseStrategyGameCourse/Assets/Scripts/HealthSystem.cs
+++ TurnBaseStrategyGameCourse/Assets/Scripts/HealthSystem.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-  [SerializeField] private int health = 100;
   public event EventHandler OnDead;
+  public event EventHandler OnDamaged;
+  [SerializeField] private int health = 100;
+  private int healthMax;
+
+  private void Awake()
+  {
+    healthMax = health;
+  }
+
 
   public void Damage(int damageAmount)
   {
@@ -16,6 +24,7 @@ public class HealthSystem : MonoBehaviour
     {
       health = 0;
     }
+    OnDamaged?.Invoke(this, EventArgs.Empty);
 
     if (health == 0)
     {
@@ -26,5 +35,10 @@ public class HealthSystem : MonoBehaviour
   private void Die()
   {
     OnDead?.Invoke(this, EventArgs.Empty);
+  }
+
+  public float GetHealthNormalized()
+  {
+    return (float)health / healthMax;
   }
 }
